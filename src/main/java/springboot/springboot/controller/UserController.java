e8838b3d32d320873bf5046d4daf53d86adbeaba
Nickolay Artemyev <Artemyev1000@gmail.com>
2023-06-17 00:06:21 +0900

    commit

--- src/main/java/springboot/springboot/controller/UserController.java
+++ src/main/java/springboot/springboot/controller/UserController.java
@@ -2,7 +2,13 @@ package springboot.springboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import springboot.springboot.model.User;
 import springboot.springboot.service.UserService;
 
