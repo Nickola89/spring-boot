36b5627248040970c6cdc50a8d76ae0b06d6c9f9
Nickolay Artemyev <Artemyev1000@gmail.com>
2023-06-22 14:19:13 +0900

    commit second

--- src/main/java/springboot/springboot/model/User.java
+++ src/main/java/springboot/springboot/model/User.java
@@ -6,7 +6,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Column;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Min;
 
 @Entity
 @Table(name = "users")
